fa0d18724f57bd2416f2f3c28fa0685d40986f8a
Marina Saad <eng8marinasaad@gmail.com>
2026-05-07 20:01:52 +0300

    section1_marina_saad

---
+++ section1.dart
@@ -0,0 +1,25 @@
+void main() {
+  int degree = 95;
+  print("Degree = $degree");
+  String grade;
+  if (degree >= 95 && degree <= 100) {
+    grade = "+A";
+  } else if (degree >= 90) {
+    grade = "-A";
+  } else if (degree >= 85) {
+    grade = "+B";
+  } else if (degree >= 80) {
+    grade = "-B";
+  } else if (degree >= 75) {
+    grade = "+C";
+  } else if (degree >= 70) {
+    grade = "-C";
+  } else if (degree >= 65) {
+    grade = "+D";
+  } else if (degree >= 60) {
+    grade = "-D";
+  } else {
+    grade = "F";
+  }
+  print("Grade = $grade");
+}
